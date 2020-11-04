3b0a20331a62dd0cd324cb76ee0dc1a5860671cb
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2020-11-04 13:36:03 -0500

    didn't install right

--- setup/learnSEM_setup.R
+++ setup/learnSEM_setup.R
@@ -1,13 +1,16 @@
 library(devtools)
 library(usethis)
 
-usethis::use_dev_package("lavaan")
-usethis::use_dev_package("semPlot")
+#usethis::use_dev_package("lavaan")
+#usethis::use_dev_package("semPlot")
 #usethis::use_dev_package("rio")
 
 library(rio)
 #introR <- import("data/introR.csv")
 #usethis::use_data(introR, overwrite = T)
+
+usethis::use_tutorial("introR", "Your First Lesson", open = interactive())
+
 
 library(roxygen2)
 roxygenize()

c0cd9182447ff7c9438455de0aba7857e19241e1
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2020-11-04 13:08:52 -0500

    adding introR tutorial

---
+++ setup/learnSEM_setup.R
@@ -0,0 +1,10 @@
+library(devtools)
+library(usethis)
+
+usethis::use_dev_package("lavaan")
+usethis::use_dev_package("semPlot")
+usethis::use_dev_package("rio")
+
+library(rio)
+#introR <- import("data/introR.csv")
+#usethis::use_data(introR)
